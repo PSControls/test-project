0a13198863773061eef7d9c34866ad2c0060772b
PSControls <gray.oxley@martinrea.com>
2024-05-31 14:47:16 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
-mkdir $HOME/test-project-container
+mkdir -p $HOME/test-project-container
 cd $HOME/test-project-container
-curl -o - $HOME/test-project-container/Dockerfile https://raw.githubusercontent.com/PSControls/test-project/main/Dockerfile ?token=$(date +%s)
+curl -o $HOME/test-project-container/Dockerfile https://raw.githubusercontent.com/PSControls/test-project/main/Dockerfile 
 docker build --no-cache -t node-red-project .
 docker run --name nr-container1 -p 1880:1880 node-red-project
